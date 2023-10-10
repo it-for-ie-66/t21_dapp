ab586eb9e53c5173331708548fb65ba4452a4e3b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-10-10 17:01:21 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ import { formatUnits } from "viem";
 import { useContractRead, useBalance } from "wagmi";
 import { useState } from "react";
 
+const urlMyAccount = `https://sepolia.etherscan.io/address/${MY_ACCOUNT_ADDRESS}`;
+const urlSecret = `https://sepolia.etherscan.io/address/${SECRET_CONTRACT_ADDRESS}`;
+const urlToken = `https://sepolia.etherscan.io/address/${TOKEN_CONTRACT_ADDRESS}`;
+const urlNFT = `https://sepolia.etherscan.io/address/${NFT_CONTRACT_ADDRESS}`;
+
 function App() {
   const [NFTimage, setNFTImage] = useState<string>("");
 
@@ -44,23 +49,26 @@ function App() {
     },
   });
 
+  const displayBalance = useBal.isLoading
+    ? "...loading"
+    : `${parseFloat(useBal.data?.formatted || "0").toFixed(2)} (SepoliaETH)`;
+  const displaySecret = useReadSecret.isLoading
+    ? "...loading"
+    : useReadSecret.data || "";
+  const displayToken = useReadTokenBalance.isLoading
+    ? "...loading"
+    : `${parseFloat(formatUnits(useReadTokenBalance.data || 0n, 18)).toFixed(
+        2
+      )} ${TOKEN_SYMBOL}`;
+
   return (
     <>
       <h1>My Blockchain Assets</h1>
 
       <div>
         <h2>My account balance</h2>
-        <div>
-          {useBal.isLoading
-            ? "...loading"
-            : `${parseFloat(useBal.data?.formatted || "0").toFixed(
-                2
-              )} (SepoliaETH)`}
-        </div>
-        <a
-          href={`https://sepolia.etherscan.io/address/${MY_ACCOUNT_ADDRESS}`}
-          target="_blank"
-        >
+        <div>{displayBalance}</div>
+        <a href={urlMyAccount} target="_blank">
           Link
         </a>
         <hr />
@@ -68,13 +76,8 @@ function App() {
 
       <div>
         <h2>My secret</h2>
-        <div>
-          {useReadSecret.isLoading ? "...loading" : useReadSecret.data || ""}
-        </div>
-        <a
-          href={`https://sepolia.etherscan.io/address/${SECRET_CONTRACT_ADDRESS}`}
-          target="_blank"
-        >
+        <div>{displaySecret}</div>
+        <a href={urlSecret} target="_blank">
           Link
         </a>
         <hr />
@@ -82,17 +85,8 @@ function App() {
 
       <div>
         <h2>My Token</h2>
-        <div>
-          {useReadTokenBalance.isLoading
-            ? "...loading"
-            : `${parseFloat(
-                formatUnits(useReadTokenBalance.data || 0n, 18)
-              ).toFixed(2)} ${TOKEN_SYMBOL}`}
-        </div>
-        <a
-          href={`https://sepolia.etherscan.io/address/${TOKEN_CONTRACT_ADDRESS}`}
-          target="_blank"
-        >
+        <div>{displayToken}</div>
+        <a href={urlToken} target="_blank">
           Link
         </a>
         <hr />
@@ -101,17 +95,13 @@ function App() {
       <div>
         <h2>My NFT</h2>
         <div>
-          {userReadNFT.isLoading ? (
+          {userReadNFT.isLoading || !NFTimage ? (
             "...loading"
           ) : (
             <img src={NFTimage} style={{ maxHeight: "30vh" }} />
           )}
         </div>
-
-        <a
-          href={`https://sepolia.etherscan.io/address/${NFT_CONTRACT_ADDRESS}`}
-          target="_blank"
-        >
+        <a href={urlNFT} target="_blank">
           Link
         </a>
       </div>
